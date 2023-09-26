0affafe422df1020054d4a8d6e189f100129b6ac
AndreaLirosi <lirosiandrea@gmail.com>
2023-09-26 16:51:03 +0200

    primitive2

--- Settimana1/Primitive2/src/Tests.java
+++ Settimana1/Primitive2/src/Tests.java
@@ -1,13 +1,10 @@
-
 public class Tests {
     public static void main(String[] args) {
         char x = 'a';
         char y = 'b';
-       String output = "la stringa è: ";
+        String output = "la stringa è: ";
 
-       String result = output + x+y;
+        String result = output + x + y;
         System.out.println(result);
-
-
     }
 }
