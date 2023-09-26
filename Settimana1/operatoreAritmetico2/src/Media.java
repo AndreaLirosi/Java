d28e1f6542cb255d0d14acd69573a847d310b074
AndreaLirosi <lirosiandrea@gmail.com>
2023-09-26 21:26:27 +0200

    add operatore2

---
+++ Settimana1/operatoreAritmetico2/src/Media.java
@@ -0,0 +1,13 @@
+
+public class Media {
+    public static void main(String[] args) {
+        float num1 = 3;
+        float num2 = 26;
+        float num3 = 13;
+
+        System.out.println("la media delle variabili è:" +media(num1,num2,num3));
+    }
+    public static float media (float num1, float num2, float num3) {
+        return ((num1+num2+num3) / 3);
+    }
+    }
