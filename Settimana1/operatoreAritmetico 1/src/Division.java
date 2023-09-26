1dc11b40a1edb6f4630f355b37819fbb1d8a80dd
AndreaLirosi <lirosiandrea@gmail.com>
2023-09-26 21:04:28 +0200

    add Division

---
+++ Settimana1/operatoreAritmetico 1/src/Division.java
@@ -0,0 +1,11 @@
+
+public class Division {
+    public static void main(String[] args) {
+        float num1 = 27;
+        float num2 = 18;
+        System.out.println("the result is:" +division(num1, num2));
+    }
+    public static double division (float num1, float num2) {
+        return (num1 / num2);
+    }
+}
