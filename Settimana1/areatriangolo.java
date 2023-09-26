1a43a1b11d6b2dadfaca21005232afeefbac562d
AndreaLirosi <lirosiandrea@gmail.com>
2023-09-26 11:39:56 +0200

    addfile

---
+++ Settimana1/areatriangolo.java
@@ -0,0 +1,9 @@
+public class AreaTriangolo {
+    public static void main(String args[]) {
+        double base = 12;
+        double height =18;
+        double area = (base * height) / 2;
+
+        System.out.println("area is = " + area);
+    }
+}
