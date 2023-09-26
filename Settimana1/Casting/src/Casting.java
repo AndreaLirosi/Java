71331e45c4049d43526ab92bc1931e4493913642
AndreaLirosi <lirosiandrea@gmail.com>
2023-09-26 18:09:12 +0200

    Casting

---
+++ Settimana1/Casting/src/Casting.java
@@ -0,0 +1,11 @@
+
+public class Casting {
+    public static void main(String[] args) {
+    double num1 = 5.5;
+    double num2 = 7.8;
+
+    double sum = (int) (num1 + num2);
+
+    System.out.println(sum);
+    }
+}
