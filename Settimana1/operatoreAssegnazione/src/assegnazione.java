8bfe6dada083da6e1795e60ebb61568c32b5cd6c
AndreaLirosi <lirosiandrea@gmail.com>
2023-09-26 22:08:37 +0200

    addAssegnazione

---
+++ Settimana1/operatoreAssegnazione/src/assegnazione.java
@@ -0,0 +1,14 @@
+
+public class assegnazione {
+    public static void main(String[] args) {
+    float x = 8;
+    float y = 5;
+
+    System.out.println(result(x,y));
+    }
+    public static float result (float x, float y) {
+        float addx = x+2;
+        float addy = y+2;
+        return (addx * addy);
+    }
+}
