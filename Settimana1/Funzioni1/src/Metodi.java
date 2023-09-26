dc13c24d2c8c8a53285194b6c3b3917577996155
AndreaLirosi <lirosiandrea@gmail.com>
2023-09-26 18:55:22 +0200

    add Metodi

---
+++ Settimana1/Funzioni1/src/Metodi.java
@@ -0,0 +1,15 @@
+
+public class Metodi {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 8;
+
+        System.out.println("la somma di x+y è:" + somma(x, y));
+    }
+
+    public static int somma(int x, int y) {
+        int z = x + y;
+        return z;
+    }
+
+}
