239a2bc0a51d2f2db98b3b5c6a588e71d2f33060
AndreaLirosi <lirosiandrea@gmail.com>
2023-09-26 16:16:43 +0200

    esercizio testo

---
+++ Settimana1/Tests.java
@@ -0,0 +1,13 @@
+
+public class Tests {
+    public static void main(String[] args) {
+        char x = 'a';
+        char y = 'b';
+       String output = "la stringa è: ";
+
+       String result = output + x+y;
+        System.out.println(result);
+
+
+    }
+}
