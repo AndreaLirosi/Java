2faa7dc221d95a07ec51925f907cfae6a0a47aec
AndreaLirosi <lirosiandrea@gmail.com>
2023-10-02 11:42:50 +0200

    add correct exercise OperatoriLogici1

--- Settimana1/operatoriLogici1/src/Logic1.java
+++ Settimana1/operatoriLogici1/src/Logic1.java
@@ -2,21 +2,15 @@
 public class Logic1 {
     public static void main(String[] args) {
         int num1 = 32;
-        int num2 = 52;
         int limiteInf = 20;
         int limiteSup = 50;
-        System.out.println("limite inf =" +limiteInf);
-        System.out.println("limite sup =" +limiteSup);
-        System.out.println("num1 è compreso nei limiti:" + result(num1,limiteInf,limiteSup));
-        System.out.println("num2 è compreso nei limiti:" + result2(num2,limiteInf,limiteSup));
-    }
-    public static boolean result (int num1, int limiteInf,int limiteSup) {
-        return (num1>limiteInf && num1<limiteSup);
+        System.out.println(num1 + " " + "è compreso tra: " + limiteInf + " " + "e " + limiteSup + "?" + result(num1,limiteInf,limiteSup));
 
     }
-    public static boolean result2 (int num2, int limiteInf,int limiteSup) {
-        return (num2>limiteInf && num2<limiteSup);
-
+    public static boolean result (int x, int limiteInf,int limiteSup) {
+        boolean range = x >= limiteInf && x <= limiteSup;
+        return range;
     }
+
 
 }
