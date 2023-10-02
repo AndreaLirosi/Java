04432bcd5591450ec1dad4ae6b17aab834bc14f4
AndreaLirosi <lirosiandrea@gmail.com>
2023-10-02 12:41:22 +0200

    add correct CicloFor 1

--- Settimana1/ciclo For 1/src/tabelline.java
+++ Settimana1/ciclo For 1/src/tabelline.java
@@ -1,15 +1,14 @@
 
 public class tabelline {
     public static void main(String[] args) {
-        int base = 2;
-        int count = calcolatore (base); //richiamo la funzione nel main
+        int base = 7;
+        calcolatore (base); //richiamo la funzione nel main
     }
 
-    public static int calcolatore (int base) {
+    public static void calcolatore (int base) {
     //il ciclo inizia da 0 e termina quando i = 10, e aumenta di un fattore pari a base
-        for (int i=0; i <= (base*10); i = i+base) {
+        for (int i=base; i <= (base*10); i = i+base) {
             System.out.println (i); // stampa il valore i per ogni iterazione
         }
-        return base;
     }
 }
