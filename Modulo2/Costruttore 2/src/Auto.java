961b82e07d37824157427f5d265b53329c53c181
AndreaLirosi <lirosiandrea@gmail.com>
2023-10-04 12:29:40 +0200

    mod Costruttore2

--- Modulo2/Costruttore 2/src/Auto.java
+++ Modulo2/Costruttore 2/src/Auto.java
@@ -1,16 +1,39 @@
-public class Auto {
-    private final int cilindrata;
-    private final String targa;
-    private final String marca;
+public class Auto {     // creo le variabili private
+    private int cilindrata;
+    private String targa;
+    private String marca;
 
-    private final String modello;
+    private String modello;
 
+    // vado a creare i setter e i getter (che hanno la stessa sintassi)
+    //i Get mi permatto di prendere la variabile dalla classe
+    public String getTarga() {
+        return targa;
+    }
+// i set mi permettono di settare il valore della variabile, andando così, nel main,
+    // a evitare possibili errori
+    public void setTarga(String newTarga) {
+        this.targa = newTarga;
+    }
 
-    public Auto(int cilindrata, String targa, String marca, String modello) {
-        this.cilindrata = cilindrata;
-        this.targa = targa;
-        this.marca = marca;
-        this.modello = modello;
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String newMarca) {
+        this.marca = newMarca;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public void setModello(String newModello) {
+        this.modello = newModello;
+    }
+
+    public void setCilindrata(int newCilindrata) {
+        this.cilindrata = newCilindrata;
     }
 
     public int getCilindrata() {
--- Modulo2/Costruttore 2/src/Main.java
+++ Modulo2/Costruttore 2/src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        Auto car = new Auto(1200, "RM00123", "Fiat", "500");
+        Auto car = new Auto();
+        car.setCilindrata(1200);
+        car.setTarga("RM00123");
+        car.setMarca("Fiat");
+        car.setModello("500");
+
 
         System.out.println(car);
-        System.out.println(" la cilindrata è: "+ car.getCilindrata());
-
+        System.out.println(" la cilindrata è: " + car.getCilindrata());
+        System.out.println(" la Targa è: " + car.getTarga());
+        System.out.println(" la Marca è: " + car.getMarca());
+        System.out.println(" il Modello è: " + car.getModello());
 
     }
 }
