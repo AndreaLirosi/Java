349d53ddd0650c777eb86fef06f1db23693a80b2
AndreaLirosi <lirosiandrea@gmail.com>
2023-10-04 09:55:45 +0200

    add Modulo2 & Costruttore 1

---
+++ Modulo2/Costruttore 1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    Student Stud = new Student("Andrea", "Lirosi", 10023);
+
+    System.out.println(Stud);
+
+
+
+    }
+}
---
+++ Modulo2/Costruttore 1/src/Student.java
@@ -0,0 +1,26 @@
+public class Student {      //creo la classe immettendo i dati di cui è composta
+    private final String name;
+    private final String surname;
+    private final int ID;
+
+    // creo un costruttore che sarebbe una funzione con lo stesso nome della classe
+    // uso this. poiché permette di accedere all'istanza.
+    public Student(String name, String surname, int ID) {
+        this.name = name;
+        this.surname = surname;
+        this.ID = ID;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", ID=" + ID +
+                '}';
+    }
+
+
+
+
+}
