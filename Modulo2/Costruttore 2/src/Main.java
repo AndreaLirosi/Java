bd857c5222bc300b6d42f6e7b208b26db90505bf
AndreaLirosi <lirosiandrea@gmail.com>
2023-10-04 10:19:51 +0200

    add Costruttore2

---
+++ Modulo2/Costruttore 2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Auto car = new Auto(1200, "RM00123", "Fiat", "500");
+
+        System.out.println(car);
+        System.out.println(" la cilindrata è: "+ car.getCilindrata());
+
+
+    }
+}
