d90ee987e331223df7dbb17fb57bd51bc9c7660f
AndreaLirosi <lirosiandrea@gmail.com>
2023-10-05 16:06:51 +0200

    mod Module2

--- Modulo2/Costruttore 1/src/Student.java
+++ Modulo2/Costruttore 1/src/Student.java
@@ -1,17 +1,17 @@
 public class Student {      //creo la classe immettendo i dati di cui è composta
     private final String name;
     private final String surname;
-    private final int ID;
+    private final int id;
 
     // creo un costruttore che sarebbe una funzione con lo stesso nome della classe
     // uso this. poiché permette di accedere all'istanza.
-    public Student(String name, String surname, int ID) {
+    public Student(String name, String surname, int id) {
         this.name = name;
         this.surname = surname;
-        this.ID = ID;
+        this.id = id;
     }
-    public int identify () {
-        return ID;
+    public int getID () {
+        return id;
     }
 
     @Override
@@ -19,7 +19,7 @@ public class Student {      //creo la classe immettendo i dati di cui è compost
         return "Student{" +
                 "name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
-                ", ID=" + ID +
+                ", ID=" + id +
                 '}';
     }
 
