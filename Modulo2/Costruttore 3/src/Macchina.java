7f71e9876c95f1fd4cf337d6b9bdff1d422fa1d0
AndreaLirosi <lirosiandrea@gmail.com>
2023-10-05 10:28:58 +0200

    add Costruttore3

---
+++ Modulo2/Costruttore 3/src/Macchina.java
@@ -0,0 +1,32 @@
+public class Macchina {
+    private final int cilindrata;
+    private final String targa;
+    private final String marca;
+
+    private final String modello;
+
+    public Macchina(int cilindrata, String targa, String marca, String modello) {
+        this.cilindrata = cilindrata;
+        this.targa = targa;
+        this.marca = marca;
+        this.modello = modello;
+    }
+
+    public String getTarga() {
+        return targa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public int getCilindrata() {
+        return cilindrata;
+    }
+
+
+}
