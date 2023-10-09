4481410dae2f577284fe590b1f29b6d63160b9ee
AndreaLirosi <lirosiandrea@gmail.com>
2023-10-09 11:13:28 +0200

    fix

--- Settimana1/ArrayMulti 2/src/Main.java
+++ Settimana1/ArrayMulti 2/src/Main.java
@@ -1,24 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        int[][] matrice = { {1, 2, 3}, {4, 5, 6}};
+        int[][] matrice = {
+                {1, 2, 3},
+                {4, 5, 6}};
         //stampa matrice di partenza
         creaMatrice(matrice);
-    //nuova matrice invertita dalla funzione InvertiColonneRighe
+//nuova matrice invertita dalla funzione InvertiColonneRighe
         int [][] nueMatrix = invertiColonneRighe(matrice);
-    //Mostra il risultato
-        System.out.println("Matrice risultato:");
-        creaMatrice(nueMatrix);
+
+        if (righeUguali(matrice)) {
+//Mostra il risultato
+            System.out.println("Matrice risultato:");
+            creaMatrice(nueMatrix);
+        } else {
+            System.out.println("The matrix has different dimensions for the rows.");
+        }
 
     }
-    //Funzione che crea la matrice di partenza e stampa a schermo
-    public static void creaMatrice (int [][] mat) {
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j<mat[i].length; j++){
-                System.out.print(mat[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+
     //poichè la matrice di partenza ha 2 righe (num di {}) e 3 colonne (lunghezza delle{})
     //imposto e colonne e le righe di modo da poterle invertire nel nuovo array
     public static int[][] invertiColonneRighe (int [][] mat) {
@@ -34,6 +33,48 @@ public class Main {
             }
         }
         return newMatrix;
+    }
+    //dobbiamo verificare se la matrice ha lo stesso numero di elementi per ogni riga
+    public static boolean righeUguali (int [][] matrix) {
+        //partendo dalla riga 0, andiamo a girare su ognuna delle righe della matrice
+        int righe = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            righe++;
+        }
+    //useremo un array che avrà una sola dimensione (lunga quanto le righe della matrice)
+    //alla quale assegneremo un valore ad ogni elemento pari al numero di elementi di ogni riga di matrix
+        int [] elementiOgniColonna = new int[righe];
+    // nel ciclo andiamo ad assegnare, quindi, un valore all'iesimo elemento dell'array,
+    //pari al numero di elementi di ogni sottomatrice (cioè matrix[i].length)
+        for (int i =0; i < matrix.length; i++) {
+            elementiOgniColonna[i] = matrix[i].length;
+        }
+
+        return tutteUguali (elementiOgniColonna);
+    }
+    // qui andiamo a gestire l'array creato sopra e andiamo a verificare se gli elementi
+    //sono tutti uguali.
+public static boolean tutteUguali (int [] arr) {
+    if (arr == null || arr.length == 0) {   //se l'array ha valore null o è uguale a 0 darà falso
+        return false;
+    }
+    for (int i = 1; i< arr.length; i++) {
+        if (arr[0] != arr[i]) {
+            return false;
+        }
+
+    }
+    return true;
+
+}
+    //Funzione che crea la matrice di partenza e stampa a schermo
+    public static void creaMatrice (int [][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j<mat[i].length; j++){
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
 
