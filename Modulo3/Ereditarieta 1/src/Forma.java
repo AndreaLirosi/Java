4d2d474084ad24ff88e839cd9816f4456c1b6cd2
AndreaLirosi <145360668+AndreaLirosi@users.noreply.github.com>
2023-10-09 12:35:02 +0200

    Delete Modulo3/Ereditarieta 1 directory

--- Modulo3/Ereditarieta 1/src/Forma.java
+++
@@ -1,5 +0,0 @@
-public class Forma {
-    public int calcolaArea () {
-    return 0;
-    }
-}
--- Modulo3/Ereditarieta 1/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-    Rettangolo rettangolo = new Rettangolo(10, 5);
-    int area = rettangolo.calcolaArea();
-    System.out.println("l'area del rettangolo è: " + area);
-    }
-}
