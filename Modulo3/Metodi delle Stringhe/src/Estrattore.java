73f4b0926048d5f6a0a49eb08f80883dc1542d71
AndreaLirosi <lirosiandrea@gmail.com>
2023-10-13 10:36:22 +0200

    mod Metodi delle Stringhe

--- Modulo3/Metodi delle Stringhe/src/Estrattore.java
+++ Modulo3/Metodi delle Stringhe/src/Estrattore.java
@@ -1,29 +1,20 @@
 // creiamo una classe che nominiamo Estrattore
 public class Estrattore {
-    // istanziamo le variabili che poi metteremo nel costruttore
-    private String testo;
-    private int index;
-    public Estrattore (String testo, int index) {
-    this.testo = testo;
-    this.index = index;
-    }
     // successivamente andiamo a formare il metodo:
 // il metodo si struttura in un if, dove verifichiamo se l'indice è compreso nel testo
-    public String searchUnicode (String test, int idx) {
+    public static String searchUnicode (String test, int idx) {
         if (idx >= 0 && idx < test.length()) {
             // dopodiché trasformiamo, grazie ai metodi delle stringhe il valore di idx
     // nel corrispettivo char; usando charAt.
-            char unicodeChar = test.charAt(idx);
+            int unicodeChar = test.charAt(idx);
     //creiamo un oggetto risultato dove mettiamo la frase da stampare.
             StringBuilder result = new StringBuilder();
-            result.append("Il carattere all'indice ");
-            result.append(idx);
-            result.append(" è: ");
-            result.append(unicodeChar);
+            result.append("Il carattere all'indice ").append(idx)
+                    .append(" è: ").append(unicodeChar);
 // ci deve tornare una stringa
             return result.toString();
     }
-        else return "Error";
+        return "Error";
 }
 
 }
--- Modulo3/Metodi delle Stringhe/src/Main.java
+++ Modulo3/Metodi delle Stringhe/src/Main.java
@@ -3,10 +3,8 @@ public class Main {
     // generiamo il testo e impostiamo l'indice
         String testo = "Testo di Prova";
         int index = 7;
-// creo l'oggetto con i parametri dati sopra di modo da inserirli nel metodo
-        Estrattore estratto = new Estrattore(testo,index);
 // result è quindi uguale al metodo in Estrattore ma con i parametri di estratto.
-        String result = estratto.searchUnicode(testo,index);
+        String result = Estrattore.searchUnicode(testo,index);
         System.out.println(result);
     }
 }
