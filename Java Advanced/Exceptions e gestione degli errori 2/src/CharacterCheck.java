30499d34ce80dfb928965b2288628ec2f85868f9
AndreaLirosi <lirosiandrea@gmail.com>
2023-10-20 15:32:05 +0200

    mod Exercie errori2

--- Java Advanced/Exceptions e gestione degli errori 2/src/CharacterCheck.java
+++ Java Advanced/Exceptions e gestione degli errori 2/src/CharacterCheck.java
@@ -1,27 +1,28 @@
 public class CharacterCheck {
 
     public static void main(String[] args) {
-        char test = '7';
-
-        chekChar(test);
+        char test = 'g';
+        manageError(test);
     }
 
-    public static void checkIfANumber(char test) throws Exception {
+    public static boolean checkIfANumber(char test) throws Exception {
         if (test >= '0' && test <= '9') {
-    // se vero prende l'eccezione che viene rimandato nel try e elaborato nel catch
-            System.out.println("Il carattere è un numero.");
+            // se vero prende l'eccezione che viene rimandato nel try e elaborato nel catch
+            return true;
         } else {
             throw new Exception("Il carattere non è un numero");
         }
     }
-    public static void chekChar (char car) {
+
+    public static void manageError(char car) {
         // andiamo a creare una gestione dell'errore, nel caso in cui il char sia un numero.
         //nel try andiamo ad inserire il richiamo della funzione.
         try {
             checkIfANumber(car);
+            System.out.println("Il carattere è un numero.");
             // il catch prenderà quindi in esame l'eccezione che manderà sul terminale
         } catch (Exception e) {
-            System.out.println("L'Eccezione: " + e);
+            System.out.println("Eccezione: " + e.getMessage());
         }
     }
 
