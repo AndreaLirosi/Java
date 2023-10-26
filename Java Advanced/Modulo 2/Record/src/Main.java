0ac56aba84f24613e563470fb7e946daf02cfec4
AndreaLirosi <lirosiandrea@gmail.com>
2023-10-26 16:53:27 +0200

    add Record

---
+++ Java Advanced/Modulo 2/Record/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Point punto1 = new Point(2,8);
+
+        System.out.println("la y del punto 1 è: \n" + punto1.y());
+        System.out.println("la x del punto 1 è: \n" + punto1.x());
+    }
+}
